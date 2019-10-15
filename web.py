b1be905064f28b2549b4a691459e1962b72bc85e
sp0002 <47800471+sp0002@users.noreply.github.com>
2019-10-15 21:28:48 +0800

    added minus

--- web.py
+++ web.py
@@ -2,9 +2,11 @@ from flask importFlask, render template, url_for, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('plus', methods=["GET", "POST"])
 def addition():
@@ -19,6 +21,21 @@ def addition():
         for i in range(b):
             c++
         return render_template('plused.html', a=a, b=b, c=c)
+
+    
+@app.route('minus', methods=["GET", "POST"])
+def minus():
+    if request.method == "GET":
+        return render_template('plus.html')
+    elif request.method == "POST":
+        a = request.from.get('minus1', 0)
+        b = request.from.get('minus2', 0)
+        c =0
+        for i in range(a):
+            c++
+        for i in range(b):
+            c--
+        return render_template('minused.html', a=a, b=b, c=c)
 
 
 if __name__ = "__main__":
