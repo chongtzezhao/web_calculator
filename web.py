6656092abeede58db2b19fdcf27428155b78db35
sp0002 <47800471+sp0002@users.noreply.github.com>
2019-10-15 10:36:34 +0800

    created web.py
    starting python flask file for calculator web app

---
+++ web.py
@@ -0,0 +1,25 @@
+from flask importFlask, render template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('plus', methods=["GET", "POST"])
+def addition():
+    if request.method == "GET":
+        return render_template('plus.html')
+    elif request.method == "POST":
+        a = request.from.get('plus1', 0)
+        b = request.from.get('plus2', 0)
+        c =0
+        for i in range(a):
+            c++
+        for i in range(b):
+            c++
+        return render_template('plused.html', a=a, b=b, c=c)
+
+
+if __name__ = "__main__":
+        flask.run(debug==TRUE)
